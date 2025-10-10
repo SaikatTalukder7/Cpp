1f33a72f14628b5aabd23afc879450d45427c4b7
Saikat Talukder <saikat60mu@gmail.com>
2025-10-10 23:02:44 +0600

    Add files via upload

---
+++ STL/Vector/3. size push back.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    cout << vec.size() << endl;
+    return 0;
+}
