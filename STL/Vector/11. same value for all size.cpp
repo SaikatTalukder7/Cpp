1ecf79c61e74941b2bf14b8309cb0404e1abd194
Saikat Talukder <saikat60mu@gmail.com>
2025-10-10 23:04:38 +0600

    Add files via upload

---
+++ STL/Vector/11. same value for all size.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vec(3,10); //first size then value.
+
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
