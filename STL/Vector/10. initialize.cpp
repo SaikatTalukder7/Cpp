609bfd6b8b839da44c7ce1bc6b9ab52b7eac38ab
Saikat Talukder <saikat60mu@gmail.com>
2025-10-10 23:04:26 +0600

    Add files via upload

---
+++ STL/Vector/10. initialize.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vec={1,2,3,4,5};
+
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
