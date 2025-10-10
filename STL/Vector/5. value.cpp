61f761ab05cb70a274583aadd99cf5484e08243f
Saikat Talukder <saikat60mu@gmail.com>
2025-10-10 23:03:18 +0600

    Add files via upload

---
+++ STL/Vector/5. value.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+
+    for(int val : vec)
+    {
+        cout<<val<<" ";
+    }
+    return 0;
+}
